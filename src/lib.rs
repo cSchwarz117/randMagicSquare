f427438812faadcbbb5ba6a70d93d459eaf2e475
cSchwarz117 <cschwarz@pdx.edu>
2020-03-12 20:22:42 -0700

    Add 7x7 and 5x5 ez squares and tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,43 @@ use array2d::Array2D;
 extern crate rand;
 use rand::Rng;
 
+
+pub fn fill_five_ez() -> Array2D<i64> {
+    let mut two_d = Array2D::filled_with(0, 5, 5);
+    let mut m = 0;
+    let mut n = 2;
+
+    for x in 1..26 {
+        if two_d[(m, n)] != 0 {
+            m = m + 2;
+            n = n + 4;
+        }
+        if m > 4 {
+            m = m - 5;
+        }
+        if n > 4 {
+            n = n - 5;
+        }
+
+        two_d[(m, n)] = x;
+
+        m = m + 4;
+        n = n + 1;
+
+        if m > 4 {
+            m = m - 5;
+        }
+        if n > 4 {
+            n = n - 5;
+        }
+    }
+
+    return two_d;
+}
+
+
+
+
 pub fn fill_five() -> Array2D<i64> {
     let mut two_d = Array2D::filled_with(0, 5, 5);
     let mut rng = rand::thread_rng();
@@ -36,6 +73,38 @@ pub fn fill_five() -> Array2D<i64> {
     return two_d;
 }
 
+pub fn fill_seven_ez() -> Array2D<i64> {
+    let mut two_d = Array2D::filled_with(0, 7, 7);
+    let mut m = 0;
+    let mut n = 3;
+
+    for x in 1..50 {
+        if two_d[(m, n)] > 0 {
+            m = m + 2;
+            n = n + 6;
+        }
+        if m > 6 {
+            m = m - 7;
+        }
+        if n > 6 {
+            n = n - 7;
+        }
+
+        two_d[(m, n)] = x;
+
+        m = m + 6;
+        n = n + 1;
+
+        if m > 6 {
+            m = m - 7;
+        }
+        if n > 6 {
+            n = n - 7;
+        }
+    }
+
+    return two_d;
+}
 
 pub fn fill_seven() -> Array2D<i64> {
     let mut two_d = Array2D::filled_with(0, 7, 7);
--- tests/t_square.rs
+++ tests/t_square.rs
@@ -11,9 +11,10 @@ fn print_square_five() {
         println!();
     }
 }
+
 #[test]
 fn print_square_seven() {
-    let filled = fill_seven();
+    let filled = fill_seven_ez();
     println!("All elements:");
     for row_iter in filled.rows_iter() {
         for element in row_iter {
@@ -23,6 +24,26 @@ fn print_square_seven() {
     }
 }
 
+#[test]
+fn row_col_sum_five_ez() {
+    let check = fill_five_ez();
+    let mut sum = 0;
+    for x in 0..5 {
+        for y in 0..5 {
+            sum += check[(x, y)];
+        }
+        assert_eq!(65, sum);
+        sum = 0;
+    }
+
+    for x in 0..5 {
+        for y in 0..5 {
+            sum += check[(y, x)];
+        }
+        assert_eq!(65, sum);
+        sum = 0;
+    }
+}
 
 #[test]
 fn row_col_sum_five() {
@@ -45,6 +66,26 @@ fn row_col_sum_five() {
     }
 }
 
+#[test]
+fn row_col_sum_seven_ez() {
+    let check = fill_seven_ez();
+    let mut sum = 0;
+    for x in 0..7 {
+        for y in 0..7 {
+            sum += check[(x, y)];
+        }
+        assert_eq!(175, sum);
+        sum = 0;
+    }
+
+    for x in 0..7 {
+        for y in 0..7 {
+            sum += check[(y, x)];
+        }
+        assert_eq!(175, sum);
+        sum = 0;
+    }
+}
 #[test]
 fn row_col_sum_seven() {
     let check = fill_seven();
